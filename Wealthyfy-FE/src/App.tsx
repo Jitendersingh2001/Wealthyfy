ee03573f613a0c983a848c8b22bad5c3c6683d65
Jitender Singh <jitendersingh3549@gmail.com>
2025-10-22 12:07:33 +0530

    add the google font in main by @jitender.singh

--- Wealthyfy-FE/src/App.tsx
+++ Wealthyfy-FE/src/App.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button"
-
+import HomePage from './pages/Home';
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <HomePage />
     </div>
   )
 }
