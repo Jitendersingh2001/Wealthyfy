ff0d5fb6f3e71f88d9e9184547b04502ba9de501
Jitender Singh <jitendersingh3549@gmail.com>
2025-10-22 15:52:59 +0530

    create a confif file in main by @jitender.singh

---
+++ Wealthyfy-FE/src/config/config.ts
@@ -0,0 +1,8 @@
+// config.ts
+export const config = {
+  keyclock: {
+    url: import.meta.env.VITE_KEYCLOAK_URL,
+    realm: import.meta.env.VITE_KEYCLOAK_REALM,
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+  },
+};
